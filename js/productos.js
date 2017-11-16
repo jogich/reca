25a49c2a4f6103e60a4d5376328fab7e807d1727
ToniCosta <ToniCosta96@gmail.com>
2017-11-16 10:46:06 +0100

    terminada página de producto

--- js/productos.js
+++ js/productos.js
@@ -42,7 +42,7 @@ function cargarProductos(producto, numProductos){
 
 			// Enlace que contiene la imagen del producto
 			aProducto = document.createElement("a");
-			aProducto.href = "producto.html?id=1";
+			aProducto.href = "producto.html?id="+productos[i].id;
 			aProducto.classList.add("d-block");
 			aProducto.classList.add("mb-4");
 
